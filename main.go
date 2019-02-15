190e3158bc6c59c1477d3662feb6585def664c65
Thomas Boerger <thomas@webhippie.de>
2019-02-15 10:24:38 +0100

    Add a reliable version and integrate libsonnet definition

--- main.go
+++ main.go
@@ -8,16 +8,15 @@ import (
 )
 
 var (
-	version = "0.0.0"
-	build   = "0"
+	version = "unknown"
 )
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "irc plugin"
 	app.Usage = "irc plugin"
-	app.Version = fmt.Sprintf("%s+%s", version, build)
 	app.Action = run
+	app.Version = version
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:   "prefix",
